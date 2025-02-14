9b43f6b98f5c57fe33321c548c62425239bd0be6
Adam <adam.fiedko@gmail.com>
2025-02-14 14:30:33 +0100

    mend

--- script.js
+++ script.js
@@ -57,20 +57,20 @@ function displayDog() {
 }
 
 // Function to display the cat-heart.gif
-function displayCatHeart() {
+function displayDogSecond() {
     // Clear existing content in the image container
     document.getElementById('image-container').innerHTML = '';
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for the cat-heart
-    var catHeartImage = new Image();
+    var dogSecondImage = new Image();
     // Set the source (file path) for the cat-heart image
-    catHeartImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
+    dogSecondImage.src = 'cat-heart.gif'; // Assuming the cat-heart image is named "cat-heart.gif"
     // Set alternative text for the image (for accessibility)
-    catHeartImage.alt = 'Cat Heart';
+    dogSecondImage.alt = 'Cat Heart';
     // When the cat-heart image is fully loaded, add it to the image container
-    catHeartImage.onload = function() {
-        imageContainer.appendChild(catHeartImage);
+    dogSecondImage.onload = function() {
+        imageContainer.appendChild(dogSecondImage);
         // Hide the options container
         document.getElementById('options').style.display = 'none';
     };
